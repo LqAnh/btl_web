15a702e9366365f8f700ba7671e1ec7b8033d175
LqAnh <72133178+LqAnh@users.noreply.github.com>
2021-12-12 16:37:33 +0700

    update
    searchbox

--- src/components/common/SearchTour.jsx
+++ src/components/common/SearchTour.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 
-export const SearchTour = ({className = "search-tool"}) => {
+export const SearchTour = ({ className = "search-tool" }) => {
     const [place, setPlace] = useState('')
     const [enddate, setEnddate] = useState('')
     const [startDate, setStartDate] = useState('')
@@ -23,29 +23,29 @@ export const SearchTour = ({className = "search-tool"}) => {
             <div className="option">
                 <span><i className="fas fa-map-marker-alt" /></span>
                 <div className="option-text">
-                    <p>Điểm đến</p>
-                    <input type="text" placeholder="Bạn muốn đi đâu?" onChange={(event)=>setPlace(event.target.value)} />
+                    <p>Nơi khởi hành</p>
+                    <input type="text" placeholder="Nhập để tìm kiếm" onChange={(event) => setPlace(event.target.value)} />
                 </div>
             </div>
             <div className="option">
                 <span><i className="fas fa-calendar" /></span>
                 <div className="option-text">
                     <p>Ngày đến</p>
-                    <input type="date" placeholder="Chọn ngày" onChange={(event)=>setStartDate(event.target.value)}/>
+                    <input type="date" placeholder="Chọn ngày" onChange={(event) => setStartDate(event.target.value)} />
                 </div>
             </div>
             <div className="option">
                 <span><i className="fas fa-calendar" /></span>
                 <div className="option-text">
                     <p>Ngày về</p>
-                    <input type="date" placeholder="Chọn ngày" onChange={(event)=>setEnddate(event.target.value)}/>
+                    <input type="date" placeholder="Chọn ngày" onChange={(event) => setEnddate(event.target.value)} />
                 </div>
             </div>
             <div className="option">
                 <span><i className="fas fa-user-friends" /></span>
                 <div className="option-text">
                     <p>Số khách</p>
-                    <input type="text" placeholder="Thêm người" onChange={(event)=>setSeat(event.target.value)}/>
+                    <input type="text" placeholder="Thêm người" onChange={(event) => setSeat(event.target.value)} />
                 </div>
             </div>
             <div className="search-icon" onClick={submit}>
